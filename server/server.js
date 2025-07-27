e2fde7fea8f0d038f87b4f2632f03ca070015fad
Ramla Eman <ramla.eman.ra@gmail.com>
2025-07-27 20:43:27 +0500

    commit the origin line in server.js:

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const allowedOrigins =['http://localhost:5173']
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({credentials:true, origin: allowedOrigins}))
+app.use(cors({credentials:true, }))
 
 // api end point
 app.get('/', (req, res) => {
